78313690da25bf1244f15fca6a8a64037f23fe2f
JMarcan <jan.marcan@protonmail.com>
2020-06-12 12:34:14 +0200

    feat: define network architecture in models.py

--- models.py
+++ models.py
@@ -1,10 +1,9 @@
-## TODO: define the convolutional neural network architecture
+# Define the convolutional neural network architecture
 
 import torch
 from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-# can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
 
@@ -13,25 +12,48 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        '''
+        Initialize all layers of this CNN. The requirements were:
+            1. This network takes in a square (same width and height), grayscale image as input
+            2. It ends with a linear layer that represents the keypoints
+            it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
+        '''
+        # First convolutional layer
         self.conv1 = nn.Conv2d(1, 32, 5)
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        # First Max-pooling layer
+        self.pool1 = nn.MaxPool2d(2, 2)
         
-
+        # Fully connected layer
+        self.fc1 = nn.Linear(128, 136)
+        
         
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        ''' 
+        Executes feed forward
+          
+        Args:
+            x: grayscale image as input to be analyzed by the network 
+            
+        Returns:
+            x: output of the network
+        '''
         
+        ## Definition of the feedforward behavior of this model
         
-        # a modified x, having gone through all the layers of your model, should be returned
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = self.pool1(x)
+        
+        # Fully connected layer
+        # prep for linear layer by flattening the feature maps into feature vectors
+        x = x.view(x.size(0), -1)
+        # Linear Layer
+        x = self.fc1(x)
+        
+        x = F.relu(x)
+        x = self.drop1(x)
+        
+        # Final output
         return x
