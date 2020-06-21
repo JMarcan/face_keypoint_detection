44b8f1f63b6fa7981e29d4445d930483a80d45da
JMarcan <jan.marcan@protonmail.com>
2020-06-21 19:31:58 +0200

    feat: add extra layers to the CNN network to improve performance

--- models.py
+++ models.py
@@ -19,15 +19,25 @@ class Net(nn.Module):
             it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
         '''
-        # First convolutional layer
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        # The network expects input image 224x224
         
-        # First Max-pooling layer
+        # First Convolutional layer + Max-pooling layer
+        self.conv1 = nn.Conv2d(1, 32, 5) # 1 as it's greyscale image (1 channel,  )
         self.pool1 = nn.MaxPool2d(2, 2)
         
-        # Fully connected layer
-        self.fc1 = nn.Linear(387200, 136)
-        self.drop1 = nn.Dropout(p=0.5)
+        # Second Convolutional layer + Max-pooling layer
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+
+        
+        # First Fully connected layer
+        self.fc1 = nn.Linear(179776, 3000) 
+        
+        # Second Fully connected layer
+        self.fc2 = nn.Linear(3000, 500) 
+        
+        # Third  Fully connected layer
+        self.fc3 = nn.Linear(500, 136) 
         
         
     def forward(self, x):
@@ -43,18 +53,21 @@ class Net(nn.Module):
         
         ## Definition of the feedforward behavior of this model
         
-        x = self.conv1(x)
-        x = F.relu(x)
-        x = self.pool1(x)
+        # first conv/relu + pool layer
+        x = self.pool1(F.relu(self.conv1(x)))
+        # second conv/relu + pool layer
+        x = self.pool2(F.relu(self.conv2(x)))
         
         # Fully connected layer
         
         # prep for linear layer by flattening the feature maps into feature vectors
         x = x.view(x.size(0), -1) 
-            
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.drop1(x)
+        # First fully connected layer   
+        x = F.relu(self.fc1(x))
+        # Second fully connected layer   
+        x = F.relu(self.fc2(x))
+        # Third fully connected layer   
+        x = self.fc3(x)
         
         # Final output
         return x
